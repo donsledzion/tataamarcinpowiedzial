902361fecc320b531e4b2b35efcb04c06c0dc751
donsledzion <adam@ulinia8.pl>
2021-02-24 23:19:12 +0100

    just created .gitignore file to protect some files from public view

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return ['host' => 'mysql40.mydevil.net',
-		'user' => 'm1100_sledzislaw',
-		'password' => 'vdwJXw6fiN8OQW5NKwEx',
-		'database' => 'm1100_tataamarcinpowiedzial'
-		] ;
-
-?>
--- hasher.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pass = 'ewkamarchewka85' ;
-$hpass = password_hash($pass, PASSWORD_DEFAULT);
-echo $hpass ;
-
-?>
